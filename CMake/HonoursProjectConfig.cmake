4f101c05bd46ac3403d49aa3787859f55c21eeaf
Cristian Szabo <cristian.szabo@outlook.com>
2017-03-08 11:51:25 +0000

    Add dependency for project config. Make library generate DLL files.

--- CMake/HonoursProjectConfig.cmake
+++ CMake/HonoursProjectConfig.cmake
@@ -1,6 +1,13 @@
 @PACKAGE_INIT@
 
-include ("${CMAKE_CURRENT_LIST_DIR}/@CMAKE_PROJECT_NAME@-Targets.cmake")
+include (CMakeFindDependencyMacro)
 
-include ("${CMAKE_CURRENT_LIST_DIR}/ExternalProjects/Docopt")
-include ("${CMAKE_CURRENT_LIST_DIR}/ExternalProjects/OpenCL")
+find_dependency (OpenCL 1.1)
+
+add_library (OpenCL STATIC IMPORTED)
+
+set_target_properties (OpenCL PROPERTIES
+    IMPORTED_LOCATION ${OpenCL_LIBRARY}
+    INTERFACE_INCLUDE_DIRECTORIES ${OpenCL_INCLUDE_DIR})
+
+include ("${CMAKE_CURRENT_LIST_DIR}/HonoursProjectTargets.cmake")
